c43850aab69599780633ba744a9be132477f1da8
kgbcupcake <kgbcupcake@gmail.com>
2023-02-18 13:35:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CupCakeDesigns
+ 
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const{src,  dest,  watch,  series} = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+// const purgecss = require('gulp-purgecss')
+
+function buildStyles()
+{
+    return src('cupcake/**/*.scss')
+    .pipe(sass())
+    // .pipe(purgecss( {content: ['*.html'] }))
+    .pipe(dest('css'))
+}
+
+function watchTask()
+{
+    watch(['cupcake/**/*.scss', '*.html'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
